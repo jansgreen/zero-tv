6cdb6bbc5fe5fba6f81b4a5043539d45ec83a58f
Luis Pena <Luis Pena>
2020-11-25 08:12:17 -0500

    add if for word production

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -207,14 +207,14 @@ else:
     API_KEY = config('API_KEY', '')
 
 if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'jansgreen@gmail.com'
+
+else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST = 'smtp.googlemail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
-
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'jansgreen@gmail.com'
